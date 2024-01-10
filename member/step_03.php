1fb3817c3f4e66dfba5df513f1ec3e050c6061b8
jeonni <jeonniu@naver.com>
2024-01-10 17:33:14 +0900

    Fix: Header, Footer, include

--- member/step_03.php
+++ member/step_03.php
@@ -1,14 +1,11 @@
 <!-- 회원가입 3단계(회원정보입력) -->
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ko" lang="ko">
-<!--[if (IE 7)]><html class="no-js ie7" xmlns="http://www.w3.org/1999/xhtml" xml:lang="ko" lang="ko"><![endif]-->
-<!--[if (IE 8)]><html class="no-js ie8" xmlns="http://www.w3.org/1999/xhtml" xml:lang="ko" lang="ko"><![endif]-->
 
 <?php include "../app/views/includes/head.php"; ?>
 
 <?php
 session_start();
-
 $phoneNumber1 = isset($_SESSION['phone_number1']) ? $_SESSION['phone_number1'] : '';
 $phoneNumber2 = isset($_SESSION['phone_number2']) ? $_SESSION['phone_number2'] : '';
 $phoneNumber3 = isset($_SESSION['phone_number3']) ? $_SESSION['phone_number3'] : '';
